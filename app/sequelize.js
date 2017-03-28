8bc8e659c8ea3b702cdb5c0d52863ed949130623
djokicx <djokicx@gmail.com>
2017-03-27 19:44:26 -0700

    Changed sequelize.js...

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,3 +1,10 @@
 var Sequelize = require('sequelize'),
-    sequelize = new Sequelize('postgres://romqexawasdefm:617baae9b7b42807f9b664c00c747cefcecf973bff8f16a8e2159375f9d052f9@ec2-54-235-92-236.compute-1.amazonaws.com:5432/dcdo0cmvg91frn');
-module.exports = sequelize;
+       sequelize = new Sequelize('postgres://romqexawasdefm:617baae9b7b42807f9b664c00c747cefcecf973bff8f16a8e2159375f9d052f9@ec2-54-235-92-236.compute-1.amazonaws.com:5432/dcdo0cmvg91frn', 'romqexawasdefm', '617baae9b7b42807f9b664c00c747cefcecf973bff8f16a8e2159375f9d052f9',
+    {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     5432,
+      host:     'ec2-54-235-92-236.compute-1.amazonaws.com',
+      logging:  true //false
+    });
+      module.exports = sequelize;
