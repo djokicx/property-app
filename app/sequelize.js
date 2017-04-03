b33008cbe15b0f986c555f0bd3d1f64885cbbea6
spaul5 <spaul5@dons.usfca.edu>
2017-04-03 16:35:55 -0700

    changing databases

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,4 +1,4 @@
 var Sequelize = require('sequelize'),
-       sequelize = new Sequelize('postgres://romqexawasdefm:617baae9b7b42807f9b664c00c747cefcecf973bff8f16a8e2159375f9d052f9@ec2-54-235-92-236.compute-1.amazonaws.com:5432/dcdo0cmvg91frn');
+       sequelize = new Sequelize('postgres://lyuuotnbakvtav:9c4bf2e7ce9ea7a08cee8b803a9186bbf30eeb7598adf92aa5d8333602e20104@ec2-23-21-169-238.compute-1.amazonaws.com:5432/d44g0j2203c7qd');
      
 module.exports = sequelize;
