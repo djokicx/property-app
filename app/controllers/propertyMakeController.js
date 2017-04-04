fba8e731abaa6fcbf951c69e13d7dd6beab11df4
spaul5 <spaul5@dons.usfca.edu>
2017-04-03 18:34:37 -0700

    fixed creation of properties

--- app/controllers/propertyMakeController.js
+++ app/controllers/propertyMakeController.js
@@ -47,9 +47,10 @@ module.exports.create = function(req, res) {
 	});
 
     var newAddress;
-    console.log("before validating address");
 // addressValidator.match.streetAddress` -> tells the validator that you think the input should be a street address. This data makes the validator more accurate.  
 	addressValidator.validate(testAddress, addressValidator.match.streetAddress, function(err, exact, inexact) {
+        var first = exact[0];
+        
         newAddress = {
             street: first.streetNumber + " " + first.street,
             city: first.city,
